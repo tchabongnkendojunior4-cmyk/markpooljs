85b93a391bb668c9becc325ac14bae6b259b1bfc
Junior Tchabong <junior.tchabong-nkendo@epitech.eu>
2025-10-28 13:25:27 +0100

    king

---
+++ day11/task01.php
@@ -0,0 +1,15 @@
+<?php
+
+function display_menu()
+{
+    $menu = '<ul>
+          <li><a href="home.php">Home</a></li>
+          <li><a href="product.php">Products</a></li>
+          <li><a href="about.php">About us</a></li>
+          <li><a href="contact.php">Contact</a></li>
+             </ul>';
+
+    return $menu;
+}
+
+?>
